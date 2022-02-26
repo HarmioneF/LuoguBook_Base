0f23063ffc30540de3ea50eab2ca54e4ca760132
Harmione <harmionef@163.com>
2022-02-26 10:31:05 +0800

    Fix L2-7 WA

--- Part1/Chapter1/L2-7.cpp
+++ Part1/Chapter1/L2-7.cpp
@@ -6,9 +6,9 @@ int main() {
     double p;
     int q, a, b, c, d;
     cin >> p;
-    q = int(q * 10);
+    q = int(p * 10);
     a = q / 1000;       // 千位
-    b = q / 100;        // 百位
+    b = q / 100 % 10;        // 百位
     c = q / 10 % 10;    // 十位
     d = q % 10;         // 个位
     cout << d << "." << c << b << a << endl;
